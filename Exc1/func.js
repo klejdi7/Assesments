5033af4b1b85545eaaae6df5472d9564edbd2d76
Klejdi Arapi <klejdi.arapi1@gmail.com>
2025-01-15 02:16:44 +0200

    Task 1

--- Exc1/func.js
+++ Exc1/func.js
@@ -1,7 +1,8 @@
 const total = 900900;
 
 for (let i = 1; i <= total / 2; i++) {
-    if (total % i === 0) {
-        console.log(`${i} * ${total / i}`);
-    }
+	if (total % i === 0) {
+		document.getElementById('main').innerHTML += `<p>${i} * ${total / i}</p>`;
+		console.log(`${i} * ${total / i}`);
+	}
 }
